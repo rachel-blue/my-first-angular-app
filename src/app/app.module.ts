2f084824287332b051d77628449a794a22b1855b
Rachel <rachel17.22@gmail.com>
2021-01-12 12:04:40 -0700

    added my first components for success and warning alerts with styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import {SuccessComponent} from './success/success.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
-  ],
+    declarations: [
+        AppComponent,
+        ServerComponent,
+        ServersComponent,
+        WarningComponent,
+        SuccessComponent
+    ],
     imports: [
         BrowserModule,
         FormsModule
