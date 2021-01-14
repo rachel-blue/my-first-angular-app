73e530e075a295aa0ae7353ed1f4d55f47dd2078
Rachel <rachel17.22@gmail.com>
2021-01-14 13:57:59 -0700

    example databinding with forms

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,17 +13,34 @@ import { Component, OnInit } from '@angular/core';
   // selector: '.app-servers',
   // file needs html component with attribute class="app-servers"
 
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
+
+  onCreateServer(): void {
+    this.serverCreationStatus = 'Server was created! Name is: ' + this.serverName;
+  }
+
+  // onUpdateServerName(event: Event) {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
 
 }
